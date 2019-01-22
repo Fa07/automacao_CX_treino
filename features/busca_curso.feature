7023db3438edadc4ae900e27f801b1c16487df29
Fa07 <cout.fernando.a@gmail.com>
2019-01-22 15:54:35 -0200

    buscar_curso_iterasys

---
+++ features/busca_curso.feature
@@ -0,0 +1,13 @@
+#language:pt
+#utf-8
+
+Funcionalidade: Buscar Curso
+Eu Como iniciante em automação
+Quero buscar um curso por meio da automacao
+
+@buscar_curso
+Cenario: Buscar curso e validar valor
+Dado que eu esteja no site
+Quando procurar o curso
+Entao vou validar o valor
+E o nome do professor
---
+++ features/step_definitions/buscar_curso_step.rb
@@ -0,0 +1,19 @@
+Dado("que eu esteja no site") do
+  visit("http://www.iterasys.com.br")
+end
+
+Quando("procurar o curso") do
+  binding.pry
+  page.find('[id="courses-searching-2"]').click
+  page.find('[class="thim-s form-control courses-search-input"]').set('Formação em teste de software')
+  page.find('[class="ui-menu-item7816 ob-selected"]').click
+end
+
+Entao("vou validar o valor") do
+  #page.find('class="value"')
+  page.has_content?('R$4.900,00')
+end
+
+Entao("o nome do professor") do
+  page.has_content?('JOSE CORREIA')
+end
