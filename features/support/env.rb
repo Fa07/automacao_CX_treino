212b60b17830206396dadd5320f48ea7be0f1b9d
Fernando <cout.fernando.a@gmail.com>
2019-02-10 15:30:04 -0200

    Trocando driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,12 +8,12 @@ require 'pry'
 require 'site_prism'
 
 #CONFIGURANDO O DRIVER CAPYBARA
-Capybara.register_driver :selenium do |app|
+Capybara.register_driver :chromedriver do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 #SETANDO A CONFIGURAÇÃO DO DRIVER COMO PADRÃO
-Capybara.default_driver = :selenium
+Capybara.default_driver = :chromedriver
 
 #TIMEOUT PADRÃO NA EXECUÇÃO
 Capybara.default_max_wait_time = 15
